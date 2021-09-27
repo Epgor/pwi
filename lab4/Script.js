ae99c799fff93464247d279cde9ae6530ba2645e
Epgor <61190141+Epgor@users.noreply.github.com>
2021-09-27 20:40:02 +0200

    Add files via upload

---
+++ lab4/Script.js
@@ -0,0 +1,96 @@
+var result, var1, var2, sign
+var result = 69
+var memory = [0,0,0,0,0,0,0,0,0,0]
+
+	function takeNumber(x) {
+
+	document.getElementById("viewer").innerHTML += x;	
+	
+	}
+	
+	function ResNum() {
+
+	document.getElementById("viewer").innerHTML = " ";	
+	
+	}
+	
+	function equalsNum() {
+	
+	var2 = Number(document.getElementById("viewer").innerHTML);
+				switch (sign) {
+			  case 0:
+				result = (var1-var2);
+				break;
+			  case 1:
+				result = (var1+var2);
+				break;
+			  case 2:
+				 result = (var1*var2);
+				 break;
+			  case 3:
+				if (var2 == 0) {
+					alert('Nie jesteś kaskaderem-hardkorem, czyli nie dzielisz przez 0. elo nie pozdrawiam');
+					result = ' ';
+					break;
+				} else {result = (var1/var2);
+				 break; }
+			  case 4:
+				result = Math.pow(var1, var2);
+				break;
+			  case 5:
+				result = Math.sqrt(var1);	
+				break;
+			  case 6:
+				result = Math.sin(var1);
+				break;
+			  case 7:
+				result = Math.cos(var1);
+				break;
+			  case 8:
+				result = Math.tan(var1);
+				break;
+			  case 9: 
+				result = 1 / Math.tan(var1);
+				break;
+			  case 10:
+				result = (var1%var2);
+				break;
+			}
+		document.getElementById("viewer").innerHTML = result;
+	
+	}
+	function numMath(x) {
+	
+	var1 = Number(document.getElementById("viewer").innerHTML);
+	document.getElementById("viewer").innerHTML = " ";
+	
+	sign = x;
+	
+	}
+	
+	
+	function saveMemory(x) {
+	
+	if (memory[x] == 0) {
+	memory[x] = Number(document.getElementById("viewer").innerHTML);
+	} else {
+	document.getElementById("viewer").innerHTML = memory[x];
+	}
+	}
+	
+	function funcEval() {
+	try {
+	var expression = (document.getElementById("numb").value);
+     //document.getElementById("viewer").innerHTML = expression;
+	document.getElementById("viewer").innerHTML = eval(expression);
+	 }
+  catch(err) {
+    alert("Error: " + err + ".");
+  }
+  }
+  
+  	function funcInform() {
+	
+		alert("Evaluacja jest funkcją eksperymentalną! Używasz na własne ryzyko!");
+	
+  }
---
+++ lab9/data/xxxx/Lab9.js
@@ -0,0 +1,44 @@
+var bio = document.getElementById("bio");
+var turing = document.getElementById("turing");
+var jobs = document.getElementById("jobs");
+var neumann = document.getElementById("neumann");
+var torvalds = document.getElementById("torvalds");
+var gates = document.getElementById("gates");
+
+function load(name)
+{
+    var request = new XMLHttpRequest();
+    request.open('GET', 'res/' + name + '.html');
+    request.onload = function()
+    {
+       bio.innerHTML = request.responseText; 
+    };
+    request.send();
+    
+}
+
+turing.addEventListener("click", function()
+    {
+        load("turing");
+    });
+
+
+jobs.addEventListener("click", function()
+    {
+        load("jobs");
+    });
+
+neumann.addEventListener("click", function()
+    {
+        load("neumann");
+    });
+
+torvalds.addEventListener("click", function()
+    {
+        load("torvalds");
+    });
+
+gates.addEventListener("click", function()
+    {
+        load("gates");
+    });
